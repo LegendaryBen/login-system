96881b32cc1b86f6ec06fae6f7de6e0cc6ec7029
Chukwuezi Benjamin Chimaobi <benjaminchima072@gmail.com>
2023-02-26 22:49:17 +0000

    completed the ui for the login system

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home page</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <header>
+        <h1>HOME PAGE</h1>
+        <div>
+            <a href="login.php">login</a>
+            <a href="signup.php">sign up</a>
+            <!-- <a href="logout.php">logout</a> -->
+        </div>
+    </header>
+    <main>
+        <!-- <h1>WELCOME <span>BENJAMIN</span></h1> -->
+        <h1>WELCOME TO THE HOME PAGE</h1>
+    </main>
+</body>
+</html>
